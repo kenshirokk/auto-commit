cab5d0c6d64f2a8de3d815afa61424a1f67260dc
kenshirokk <iamkenshiro@live.cn>
2020-08-21 19:58:21 +0800

    init dynamic datasource

---
+++ dynamic-datasource-spring-boot-starter/src/main/java/com/kenshirokk/dynamicdatasource/DynamicDatasourceSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.kenshirokk.dynamicdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicDatasourceSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDatasourceSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ dynamic-datasource-spring-boot-starter/src/test/java/com/kenshirokk/dynamicdatasource/DynamicDatasourceSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenshirokk.dynamicdatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicDatasourceSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
